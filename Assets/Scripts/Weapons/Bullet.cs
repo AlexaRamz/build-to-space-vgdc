bae99b8d22353f8f3278b32897eefcd62df66c9f
citlalr3 <99466352+citlalr3@users.noreply.github.com>
2024-02-21 21:09:37 -0800

    Bullet now disappears on hitting an enemy

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -14,14 +14,34 @@ public class Bullet : MonoBehaviour
 
     private Rigidbody2D rb;
 
+
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, lifeTime);
     }
 
+
     private void FixedUpdate()
     {
         rb.velocity = transform.up * speed;
     }
+
+
+    // Handle bullet collisions
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        tag = other.gameObject.tag;
+
+        if(tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+        else if(tag == "Build" || tag == "Tube")
+        {
+            Debug.Log("Tube or block hit");
+            // Destroy(gameObject);
+        }
+    }
+    
 }
