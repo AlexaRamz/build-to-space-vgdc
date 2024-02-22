8d09a05eb52ac75f590dbb39f9aacbaba0071e40
AlexaRamz <ramz.alexa@gmail.com>
2024-02-21 22:46:08 -0800

    Enemy Variants with Attack

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,19 +6,24 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 10;
+    private Vector3 spawnPos;
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        spawnPos = transform.position + new Vector3(0, 0.5f, 0);
     }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
+        Debug.Log("hit");
         if (health <= 0)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            transform.position = spawnPos;
+            health = maxHealth;
         }
     }
 }
