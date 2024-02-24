a73335f83b2a5e0df8a816dc2ac636e841a674a8
VortexOfRainbows <vortexsnail12@gmail.com>
2024-02-23 18:16:36 -0800

    fix build

--- Assets/Scripts/Building/BuildingSystem.cs
+++ Assets/Scripts/Building/BuildingSystem.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using Unity.PlasticSCM.Editor.WebApi;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
