ee856619aadc22198fd84aa1b0623b2dbbdfe2cd
AngelRamr <ramz.angel11@gmail.com>
2024-04-19 20:43:05 -0700

    Added Drill, Generator, and Container
    created a resource source that the drill goes on top of, a generator that uses a constant supply of oil to power drills and other future machinery, and a container to contain resources.

---
+++ Assets/Scripts/Interaction/Interactables/OreSource.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreSource : MonoBehaviour
+{
+	public GameObject resouceType;
+	public float richness;
+}
