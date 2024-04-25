986daf5cbbd6d7ea00ce842fdfc11825259eb367
RyanLockie16 <RyanLockie16@gmail.com>
2024-04-25 13:47:08 -0700

    Implemented Breakpoints for "Sphere" labels
    Values can be adjusted based upon how fast ships travel (currently the player alone can't leave the first sphere without a ship)

--- Assets/Scripts/Quests/HeightTracker.cs
+++ Assets/Scripts/Quests/HeightTracker.cs
@@ -9,11 +9,13 @@ public class HeightTracker : MonoBehaviour
     float maximumHeight;
     TextMeshProUGUI highscoreDisplayer;
     TextMeshPro stationaryHighscoreDisplayer;
+    TextMeshPro sphereTrackerDisplayer;
 
     public Transform playerLocation; //Reference to player's position
     public RectTransform heightTracker; //Reference to UI element that tracks highest point reached
     public RectTransform stationaryHeightTracker; //Reference to Stationary Text element that tracks highest point reached - The expectation is that this will be set to a specific element for a given level
                                                     //An example of this usage would be for a notice board style of display (see QuestTest level for an example)
+    public RectTransform sphereTracker;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +25,17 @@ public class HeightTracker : MonoBehaviour
         if (heightTracker != null)
         {
             highscoreDisplayer = heightTracker.GetComponent<TextMeshProUGUI>();
-            highscoreDisplayer.text = "Highscore: 0"; //Sets initial value to 0 before anything has properly registered
+            highscoreDisplayer.text = "Highscore: 0.000000"; //Sets initial value to 0 before anything has properly registered
         }
         if (stationaryHeightTracker != null)
         {
             stationaryHighscoreDisplayer = stationaryHeightTracker.GetComponent<TextMeshPro>();
-            stationaryHighscoreDisplayer.text = "Highscore: 0"; //Sets initial value to 0 before anything has properly registered
+            stationaryHighscoreDisplayer.text = "Highscore: 0.000000"; //Sets initial value to 0 before anything has properly registered
+        }
+        if (sphereTracker != null)
+        {
+            sphereTrackerDisplayer = sphereTracker.GetComponent<TextMeshPro>();
+            sphereTrackerDisplayer.text = "Troposphere"; //Sets initial text
         }
     }
 
@@ -45,7 +52,7 @@ public class HeightTracker : MonoBehaviour
 
     void UpdateText(float newScore)
     {
-        if (newScore > 0.001) //Sets minimum threshold so value is initially 0
+        if (newScore > 0.01) //Sets minimum threshold so value is initially 0
         {
             if (highscoreDisplayer != null) //Only utilizes the set parameters for text elements
             {
@@ -56,5 +63,25 @@ public class HeightTracker : MonoBehaviour
                 stationaryHighscoreDisplayer.text = "Highscore: " + newScore;
             }
         }
+        if (sphereTracker != null) //Implementation of breakpoints could be adjusted based upon how fast spaceships are
+        {
+            if (newScore > 1000)
+            {
+                sphereTrackerDisplayer.text = "Stratosphere";
+            }
+            else if (newScore > 3000)
+            {
+                sphereTrackerDisplayer.text = "Mesosphere";
+            }
+            else if (newScore > 5000)
+            {
+                sphereTrackerDisplayer.text = "Thermosphere";
+            }
+            else if (newScore > 40000)
+            {
+                sphereTrackerDisplayer.text = "Exosphere";
+            }
+        }
     }
+
 }
