78b3c6e92edb3637ab06281d32e48216f3b26bd7
RyanLockie16 <RyanLockie16@gmail.com>
2024-05-23 21:15:50 -0700

    Initial Setup of Planets in Space Area Complete
    Need to dynamically record all Rigid Bodies in level, possibly dynamically record all planets in level, and add features for stabilization at certain orbit values

---
+++ Assets/Scripts/SpaceGravity/GravityController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    private float gravityx;
+    private float gravityy; //Records net gravity values
+    private float c; //Used in gravity calculation
+    private float currentMass = 1; //Set to 1 as a default, used to record current rigid body mass
+    private float planetMass = 2; //Set to 2 as default, used to record current planet mass
+    [SerializeField]
+    public GameObject[] planets; //Records list of all planets in level
+    [SerializeField]
+    public Rigidbody2D[] rigidBodies; //Records list of all rigid bodies in level
+    float gravitationalConstant = 19.62f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Try to dynamically record all planets
+        //Try to dynamically record all rigid bodies
+    }
+
+    //Logic to update rigid body
+    void Update()
+    {
+        foreach (Rigidbody2D currentRB in rigidBodies)
+        {
+            Transform transform = currentRB.transform; //Refers to transform of current RB
+            float gravX = 0;
+            float gravY = 0;
+            for (int i = 0; i < planets.Length; i++)
+            {
+                planetMass = planets[i].GetComponent<PlanetStats>().mass; //Sets planet mass to stored value
+                gravityx = -1 * (transform.position.x - planets[i].transform.position.x);
+                gravityy = -1 * (transform.position.y - planets[i].transform.position.y);
+                c = Mathf.Sqrt(gravityx * gravityx) + (gravityy * gravityy);
+                gravX += gravityx * gravitationalConstant * currentMass * planetMass / c / c / c;
+                gravY += gravityy * gravitationalConstant * currentMass * planetMass / c / c / c; //Accumulates grav X and Y based upon list of planets, relative to the current transform
+            }
+            currentRB.AddForce(new Vector2(gravX, gravY)); //Modifies gravity based upon planet locations - try to modify this global version if it doesnt work: Physics2D.gravity = new Vector2(gravX, gravY);
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceGravity/PlanetStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetStats : MonoBehaviour
+{
+    [SerializeField]
+    public float mass = 2; //Sets default mass value to 2 for a planet
+}
