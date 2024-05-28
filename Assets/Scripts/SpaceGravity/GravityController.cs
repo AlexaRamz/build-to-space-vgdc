c97c4b338213bfc310cd5b2f83cbe7841390c6cd
RyanLockie16 <RyanLockie16@gmail.com>
2024-05-28 15:19:57 -0700

    Initial Orbital Assist Modifications

--- Assets/Scripts/SpaceGravity/GravityController.cs
+++ Assets/Scripts/SpaceGravity/GravityController.cs
@@ -8,7 +8,7 @@ public class GravityController : MonoBehaviour
     private float gravityx;
     private float gravityy; //Records net gravity values
     private float c; //Used in gravity calculation
-    private float currentMass = 1; //Set to 1 as a default, used to record current rigid body mass
+    private float currentMass = 0.1f; //Set to 1 as a default, used to record current rigid body mass
     private float planetMass = 2; //Set to 2 as default, used to record current planet mass
     public GameObject[] planets; //Records list of all planets in level
     public Rigidbody2D[] rigidBodies; //Records list of all rigid bodies in level
@@ -32,18 +32,25 @@ public class GravityController : MonoBehaviour
             Transform transform = currentRB.transform; //Refers to transform of current RB
             float gravX = 0;
             float gravY = 0;
+            float closestRadius = 1000000000;
             for (int i = 0; i < planets.Length; i++)
             {
                 planetMass = planets[i].GetComponent<PlanetStats>().mass; //Sets planet mass to stored value
                 gravityx = -1 * (transform.position.x - planets[i].transform.position.x);
                 gravityy = -1 * (transform.position.y - planets[i].transform.position.y);
-                c = Mathf.Sqrt(gravityx * gravityx) + (gravityy * gravityy);
+                c = Mathf.Sqrt((gravityx * gravityx) + (gravityy * gravityy));
+                if (c < closestRadius)
+                {
+                    closestRadius = c;
+                }
                 gravX += gravityx * gravitationalConstant * currentMass * planetMass / c / c / c;
                 gravY += gravityy * gravitationalConstant * currentMass * planetMass / c / c / c; //Accumulates grav X and Y based upon list of planets, relative to the current transform
             }
-            float dotProduct = currentRB.velocity.x * gravX + currentRB.velocity.y * gravY;
-            if (dotProduct == 0 && (gravX != 0 || gravY != 0) && orbitalAssist) //Checks for parallel, only if orbital assist is enabled
+            float currentSpeed = Mathf.Sqrt((currentRB.velocity.x * currentRB.velocity.x) + (currentRB.velocity.y * currentRB.velocity.y));
+            if (currentSpeed < 5f && orbitalAssist && closestRadius > 7 && closestRadius < 8) //Checks for orbit conditions, maybe make the radius value be dependent on planet
             {
+                Debug.Log("Assisting");
+                currentRB.velocity = new Vector2(-gravY/100, gravX/100); //Sets velocity perpendicular to net gravity (maybe make this just for closest planet gravity)
                 //Doesn't apply gravity if moving in parallel - in order to apply orbital assist
                 //currentRB.AddForce(new Vector2(-gravY, gravX)); //Orbital assist when perpendicular to gravity (parallel)
                 //currentRB.AddForce(new Vector2(gravX, gravY)); //Modifies gravity based upon planet locations - try to modify this global version if it doesnt work: Physics2D.gravity = new Vector2(gravX, gravY);
